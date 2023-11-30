88d7bb8cd0c58b87cd72302da08ef74baee73d9b
Alim1899 <alimakhchanogli@gmail.com>
2023-11-30 23:49:17 +0400

    Made simple layout with 2 wrapped div

---
+++ src/components/UI/Layout.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import classes from './Layout.module.css'
+import logo from '../../assets/amex-logo.svg'
+const Layout = () => {
+  return (
+    <div className={classes.layout}>
+    <div className={classes.logo}>
+
+    <img src={logo} alt='logo'></img>
+    </div>
+    <div className={classes.content}>
+        <label>
+        Enter vin number
+            <input type='text'></input>
+        </label>
+    </div>
+    </div>
+  )
+}
+
+export default Layout
